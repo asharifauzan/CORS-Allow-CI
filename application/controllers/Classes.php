1d391d83bae29c38eaff888ec3b221b10b39270e
asharifauzan <fauzansaja10@gmail.com>
2020-10-12 16:44:45 +0700

    memperbaiki query melihat semua kelas

--- application/controllers/Classes.php
+++ application/controllers/Classes.php
@@ -15,11 +15,26 @@ class Classes extends Token {
   public function index_get() {
     // mengambil seluruh kelas pada databse
     $class = $this->m_classes->getClasses();
+
+    for ($i=0; $i < count($class); $i++) {
+      $students_id   = explode(',', $class[$i]['students_id']);
+      $students_name = explode(',', $class[$i]['students_name']);
+      $all_students = [];
+
+      for ($j=0; $j < count($students_id); $j++) {
+        $all_students[$j] = ['id' => $students_id[$j], 'name' => $students_name[$j]];
+      }
+
+      $class[$i]['students'] = $all_students;
+      unset($class[$i]['students_id']);
+      unset($class[$i]['students_name']);
+    }
+
     // memberikan response sukses dan mengirim daftar kelas
     $this->response([
       'status' => TRUE,
       'message' => 'success',
-      'data' => $class
+      'data' => ($class)
     ], 400);
   }
   
--- application/models/M_classes.php
+++ application/models/M_classes.php
@@ -7,18 +7,19 @@ class M_classes extends CI_Model {
   private $_tblname = 'class';
 
   public function getClasses() {
-    // mengambil jadwal kelas beserta dosen dan metkulnya
-    $this->db->select(['schedules.id', 'id_user as mahasiswa',
-                      'className as kelas', 'active',
-                      'day', 'courses.name as mata_kuliah',
-                      'id_lecturer as dosen'])
-    ->from('schedules')
-    ->join($this->_tblname, 'schedules.id_class = class.id')
-    ->join('courses', 'courses.code=schedules.id_courses')
-    ->order_by('schedules.id', 'asc');
+    $this->db->select(['class.id', 'class.className as kelas',
+                       'GROUP_CONCAT(users.id) as students_id',
+                       'GROUP_CONCAT(users.name) as students_name',
+                       'schedules.day as hari',
+                       'schedules.id_lecturer as dosen', 'courses.name as mata_kuliah'])
+             ->from($this->_tblname)
+             ->join('students', 'students.id_class = class.id')
+             ->join('users', 'students.id_user = users.id')
+             ->join('schedules', 'class.id_schedule = schedules.id')
+             ->join('courses', 'schedules.id_courses = courses.code')
+             ->group_by('class.id');
     return $this->db->get()->result_array();
   }
-
 }
 
 ?>
