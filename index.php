1ba290dd13a05bcb94bd67bcd5080343db6c7d2d
Fauzan Hibatullah Ashari <fauzansaja10gmail.com>
2020-12-22 02:18:51 +0700

    add header cors index.php

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php
+
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE');
+header('Access-Control-Allow-Headers: Content-Type, Content-Length, Accept-Encoding');
+header('Access-Control-Allow-Credentials: *');
 /**
  * CodeIgniter
  *
